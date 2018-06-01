643162fe8db1a754ab3dc53520b9da4e6a496435
NochexDevTeam <36759878+NochexDevTeam@users.noreply.github.com>
2018-06-01 13:09:06 +0100

    Update to add a button
    Update to add a button onto the redirect page in case customers have javascript disabled on the browser

--- app/code/local/NochexLTD/Nochex/Block/Redirect.php
+++ app/code/local/NochexLTD/Nochex/Block/Redirect.php
@@ -19,12 +19,21 @@ class NochexLTD_Nochex_Block_Redirect extends Mage_Core_Block_Abstract
             ->setName('nochex_checkout')
             ->setMethod('POST')
             ->setUseContainer(true);
-        foreach ($nochex->getNochexCheckoutFormFields() as $field=>$value) {
+        foreach ($nochex->getNochexCheckoutFormFields() as $field=>$value) {		
             $form->addField($field, 'hidden', array('name'=>$field, 'value'=>$value));
         }
-        $html = '<html><body>';
-        $html.= $this->__('<br /><br /><br /><br /><br /><br /><br /><br /><p align="center">You will be redirected to Nochex in a few seconds.');
-		$html.= $this->__('<br /><img src="https://ssl.nochex.com/images/magento/nochex-loader.gif" alt="Progress"/></p>');
+	
+		$submitButton = new Varien_Data_Form_Element_Submit(array(
+            'value'    => $this->__('Click here if you are not redirected within a few seconds...'),
+        ));
+        $id = "submit_to_nochex_button";
+        $submitButton->setId($id);
+        $form->addElement($submitButton);
+		
+		
+        $html = '<html><body><style>#nochex_checkout{text-align: center;}</style>';
+        $html.= $this->__('<br /><br /><br /><br /><br /><p align="center">You will be redirected to Nochex in a few seconds.');
+		$html.= $this->__('<br /><br /><img src="https://ssl.nochex.com/images/magento/nochex-loader.gif" alt="Progress"/></p>');
         $html.= $form->toHtml();
         $html.= '<script type="text/javascript">document.getElementById("nochex_checkout").submit();</script>';
         $html.= '</body></html>';
